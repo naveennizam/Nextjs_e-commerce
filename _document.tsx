49bdba3d9cac7f272b08785e5232a765f2417dc1
Naveen Nizam <82910319+naveennizam@users.noreply.github.com>
2023-08-10 12:31:07 +0500

    Update _document.tsx

--- _document.tsx
+++ _document.tsx
@@ -5,13 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-          {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
-            // eslint-disable-next-line @next/next/no-sync-scripts
-            <script
-              data-project-id="5w6L3EKdvwqvKyLtx9NSwYEB51FVI6ptXCxZDrrz"
-              src="https://snippet.meticulous.ai/v1/meticulous.js"
-            />
-          )}
+         
       </Head>
       <body>
         <Main />
